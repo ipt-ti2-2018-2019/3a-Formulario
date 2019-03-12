09661d60a4e878a2b58b0384535acca6a3fd4d3f
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2019-03-12 17:19:30 +0000

    trabalho feito na aula...

---
+++ Formulario/Formulario/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Formulario.Controllers {
+
+   public class HomeController : Controller {
+
+      // GET: Home
+      public ActionResult Index() {
+
+         // cria o ViewBag e coloca nele um valor vazio
+         ViewBag.mensagem = "";
+
+         return View();
+      }
+
+      [HttpPost]
+      public ActionResult Index(string nome, int? idade) {
+         // agora vai-se fazer alguma coisa...
+
+         string msg;
+
+         if(idade >= 0 && idade <= 130) {
+            // cria a mensagem a entregar na View
+            msg = "Olá, sou o(a) " + nome + ", e tenho " + idade + " anos...";
+         }
+         else {
+            msg = "ERRO: a idade não está bem escrita.\n" +
+                  "Escolha uma idade entre 0 e 130 anos...";
+         }
+
+         // 'envelopa' a mensagem (msg) para ser enviada para a View
+         ViewBag.mensagem = msg;
+
+         return View();
+      }
+
+
+   }
+}
